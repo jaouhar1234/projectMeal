07f5ab7c970cc31facc218c38cc8b61ec0358a1f
root <root@vps148346.vps.ovh.ca>
2019-03-06 10:35:49 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectMeal::Application.config.secret_key_base = '4fb3627bd1f931ad5a4481088c0966fe3ae9a66d238d7f689e81efe90c5974ae219b7952ead711f0df8c4ffd2c8a28146fc1d9f4998048792fa08d5e2fa5b8b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectMeal::Application.config.session_store :cookie_store, key: '_projectMeal_session'
